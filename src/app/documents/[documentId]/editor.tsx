e0fce4ae115a20e2709a94129c6c5aba6563de39
ZIAN786 <zakirzian41@gmail.com>
2025-01-08 06:12:28 +0600

    editor tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,7 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import Underline from '@tiptap/extension-underline'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
 import Table from '@tiptap/extension-table'
@@ -10,7 +11,6 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
-
 import { useEditorStore } from '@/store/use-editor-store'
 
 
@@ -50,6 +50,7 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            Underline,
             Image,
             ImageResize,
             TaskList,
