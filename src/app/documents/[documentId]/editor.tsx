43537d84a55fa01f8e0035d02cd108997d3191df
ZIAN786 <zakirzian41@gmail.com>
2025-01-31 03:44:58 +0600

    Link & Image tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -16,6 +16,7 @@ import FontFamily from '@tiptap/extension-font-family'
 import TextStyle from '@tiptap/extension-text-style'
 import { Color } from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
+import Link from '@tiptap/extension-link'
 
 
 export const Editor = () => {
@@ -58,6 +59,7 @@ export const Editor = () => {
             TextStyle,
             Color,
             Highlight.configure({ multicolor: true }),
+            Link.configure({ openOnClick: false, autolink: true, defaultProtocol:"https" }),
             Underline,
             Image,
             ImageResize,
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
@@ -16,12 +17,151 @@ import {
     ListTodoIcon,
     RemoveFormattingIcon,
     ChevronDownIcon,
-    HighlighterIcon
+    HighlighterIcon,
+    Link2Icon,
+    ImageIcon,
+    UploadIcon,
+    SearchIcon
 } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, } from "@/components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, } from "@/components/ui/dropdown-menu";
+import {Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle} from "@/components/ui/dialog";
 import { type Level } from "@tiptap/extension-heading";
 import {type ColorResult, SketchPicker} from "react-color";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from '@tiptap/extension-image'
 
+const ImageButton =() => {
+    const {editor} = useEditorStore();
+    const[isDialogOpen, setIsDialogOpen] = useState(false);
+    const [imageUrl, setImageUrl] = useState("");
+
+    const onChange = (src: string) => {
+        editor?.chain().focus().setImage({src}).run();
+        
+    };
+
+    const onUpload = () => {
+        const input = document.createElement("input");
+        input.type = "file";
+        input.accept = "image/*";
+
+        input.onchange = (e) => {
+            const file = (e.target as HTMLInputElement).files?.[0];
+            if (file) {
+                const imageUrl = URL.createObjectURL(file);
+                onChange(imageUrl);
+        };
+    }
+
+    input.click();
+    };
+
+    const handleImageUrlSubmit = () => {
+            if(imageUrl) {
+                onChange(imageUrl);
+                setIsDialogOpen(false);
+                setImageUrl("");
+        }
+}
+
+    return (
+        <>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+        <button
+        
+            className={cn(
+                "h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg=neutral-200/80 px-1.5 overflow-hidden text-sm"
+
+               )}
+        >
+            <ImageIcon className="size-4"/>
+        </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem onClick={onUpload}>
+                    <UploadIcon className="size-4 mr-2"/>
+                    Upload
+                </DropdownMenuItem>
+
+                <DropdownMenuItem onClick={() =>setIsDialogOpen(true)}>
+                    <SearchIcon className="size-4" />
+                    Paste Image URL
+                </DropdownMenuItem>
+               
+            </DropdownMenuContent>
+        </DropdownMenu>
+
+               <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Insert Image URL</DialogTitle>
+                </DialogHeader>
+                <Input 
+                placeholder="Insert Image URL"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+                onKeyDown={(e) => {
+                    if(e.key === "Enter") {
+                        handleImageUrlSubmit();
+                    }
+                }}
+                />
+
+                
+                <DialogFooter>
+                    <Button onClick={handleImageUrlSubmit}>Insert</Button>
+                </DialogFooter>
+                </DialogContent>
+               </Dialog>
+
+        </>
+
+    );
+};
+
+const LinkButton =() => {
+    const {editor} = useEditorStore();
+    const [value, setValue] = useState("");
+    const onChange = (href: string) => {
+        editor?.chain().focus().extendMarkRange("link").setLink({href}).run();
+        setValue("");
+    };
+
+    return (
+        <DropdownMenu
+        onOpenChange={(open) => {
+            if (open) {
+                setValue(editor?.getAttributes("link").href || "")
+            };
+        }}
+        >
+            <DropdownMenuTrigger asChild>
+        <button
+        
+            className={cn(
+                "h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg=neutral-200/80 px-1.5 overflow-hidden text-sm"
+
+               )}
+        >
+            <Link2Icon className="size-4"/>
+        </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-2.5 flex items-center gap-x-2">
+               <Input
+               placeholder="https://example.com"
+               value={value}
+               onChange={(e) => setValue(e.target.value)}
+               />
+               <Button onClick={() => onChange(value)}>
+                Apply
+               </Button>
+            </DropdownMenuContent>
+        </DropdownMenu>
+
+    );
+};
 const HighlightColorButton =() => {
     const {editor} = useEditorStore();
 
@@ -326,9 +466,9 @@ export const Toolbar = () => {
 
      <HighlightColorButton />
      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-      {/* TODO:Link*/}
+      <LinkButton />
 
-       {/* TODO:Image*/}
+       <ImageButton />
 
      {/* TODO:Align */}
 
